65c757778012487c81703f552d9cca86a13ec2dd
Crowdbotics <team@crowdbotics.com>
2023-08-06 10:58:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rough-dew-42680",
+  url: "https://rough-dew-42680.botics.co",
+  api: "https://rough-dew-42680.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
